a86afebae9272f1ea780619cdba98a37646ec58d
Joshua Draxten <joshuadraxten@live.com>
2021-06-06 19:09:48 -0500

    Tidy code

--- src/helpers/beautifyPrice.ts
+++ src/helpers/beautifyPrice.ts
@@ -1,14 +1,14 @@
 // $6M – $10M is much easier to read than $6,000,000 – $10,000,000
 export default function beautifyPrice(price: number, currency: string = "usd") {
-    // Detect locale to display the currencly unambiguously as USD
-    let locale = 'en-US';
-    if ( navigator.languages ) {
-        locale = navigator.languages[0];
-    }
-    return new Intl.NumberFormat(locale, {
-        style: 'currency',
-        currency,
-        minimumFractionDigits: 0,
-        notation: "compact"
-    }).format(price);
+  // Detect locale to display the currencly unambiguously as USD
+  let locale = "en-US";
+  if (navigator.languages) {
+    locale = navigator.languages[0];
+  }
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+    minimumFractionDigits: 0,
+    notation: "compact",
+  }).format(price / 100);
 }
